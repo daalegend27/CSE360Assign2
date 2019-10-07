c512ba4bb6e7a88efeba0b92bc4c5be1bb9362c2
Kobe Goldman <kobegoldman@gmail.com>
2019-10-06 18:27:38 -0700

    final commit

--- CSE360/src/cse360assign2/AddingMachine.java
+++ CSE360/src/cse360assign2/AddingMachine.java
@@ -1,36 +1,95 @@
 package cse360assign2;
 
-public class AddingMachine {
-
-	public static void main(String[]args)
-	{
-		System.out.println("hi");
-	}
-	
+public class AddingMachine 
+{
 	
 	private int total;
+	String operations = "0";
 	
-	public AddingMachine () {
+	/**
+	 * Main Method to test if program works
+	 * currently commented out
+	 * 
+	 */
+	
+	public static void main(String[]args)
+	{
+		AddingMachine mc = new AddingMachine();
+		int temp;
+		mc.add(14);
+		mc.subtract(2);
+		mc.subtract(2);
+		mc.subtract(2);
+		temp = mc.getTotal();
+		System.out.println(temp);
+		mc.subtract(5);
+		System.out.println(mc.getTotal());
+		mc.clear();
+		System.out.println(mc.getTotal());
+		
+	}
+	
+	/**
+	 * Method to 
+	 */
+	public AddingMachine () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
-		return 0;
+	/**
+	 * 
+	 * @return integer variable total
+	 */
+	public int getTotal () 
+	{
+		return total;
 	}
 	
-	public void add (int value) {
-		
+	/**
+	 * takes input parameter value and returns the arithmetic operation
+	 * total+=value
+	 * additionally, updates operations with the operation performed. 
+	 * @param value 
+	 */
+	public void add (int value) 
+	{
+		total += value;
+	    String tempValue = Integer.toString(value);
+		operations = operations + " + " + tempValue; 
 	}
 	
-	public void subtract (int value) {
+	/**
+	 * takes input parameter value and returns the arithmetic operation
+	 * total+-value
+	 * additionally, updates operations with the operation performed. 
+	 * @param value 
+	 */
+	public void subtract (int value) 
+	{
+		total-= value;
+		String tempValue = Integer.toString(value);
+		operations = operations + " - " + tempValue; 
 		
 	}
 		
-	public String toString () {
-		return "";
+	/**
+	 * method to return the string operations which holds the arithmetic
+	 * operations on the integer total. 
+	 * @return operations
+	 */
+	public String toString () 
+	{
+		return operations;
 	}
 
-	public void clear() {
+	/**
+	 * Method for cleaning variables. 
+	 */
+	public void clear() 
+	{
+		total = 0; 
+		operations = "0";
 	
 	}
 }
